478e4e8a5e76ce1a4fa51f223f86e68668e139fa
Karalama01 <70847460+Erraticoye@users.noreply.github.com>
2023-11-17 19:46:03 +0300

    Dosyalar Eklendi

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Add_programs_to_the_start_menu {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            openFileDialog2 = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Dosya Seç";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(156, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(296, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(124, 141);
+            button2.Name = "button2";
+            button2.Size = new Size(214, 50);
+            button2.TabIndex = 2;
+            button2.Text = "Oluştur";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(156, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(214, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(87, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Dosya Adı:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(156, 83);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(296, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(12, 83);
+            button3.Name = "button3";
+            button3.Size = new Size(138, 23);
+            button3.TabIndex = 6;
+            button3.Text = ".ico Dosyası Seç";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.Filter = "Icon Dosyası (*.ico)|*.ico";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(464, 198);
+            Controls.Add(button3);
+            Controls.Add(textBox3);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Başlat Menüsüne Program Ekle";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private Label label1;
+        private TextBox textBox3;
+        private Button button3;
+        private OpenFileDialog openFileDialog2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+using IWshRuntimeLibrary;
+using System.IO;
+
+namespace Add_programs_to_the_start_menu {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+        private void kısayolOluştur(string dosyaAdi, string dosyaYolu, string dosyaIconuYolu = null) {
+            string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+            string appStartMenuPath = Path.Combine(commonStartMenuPath);
+
+            if (!Directory.Exists(appStartMenuPath)) Directory.CreateDirectory(appStartMenuPath);
+
+            string shortcutLocation = Path.Combine(appStartMenuPath, dosyaAdi + ".lnk");
+            WshShell shell = new();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+            shortcut.Description = dosyaAdi;
+            if (dosyaIconuYolu != null) shortcut.IconLocation = dosyaIconuYolu;
+            shortcut.TargetPath = dosyaYolu;
+            shortcut.Save();
+            MessageBox.Show("Kısayol Başarıyla Oluşturuldu.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args != null) {
+                foreach (string arg in args) {
+                    if (arg == "-k" | arg == "--konsol") {
+                        AllocConsole();
+                        Console.WriteLine("Привет сука блять");
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (System.IO.File.Exists(textBox1.Text)) {
+                if (textBox3.Text == "") {
+                    if (textBox2.Text != "") {
+                        kısayolOluştur($"{textBox2.Text}", textBox1.Text);
+                    }
+                    else {
+                        MessageBox.Show("Hata: Dosya Adı Belirtmediniz.", "HATA!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else {
+                    if (System.IO.File.Exists(textBox3.Text)) {
+                        if (textBox2.Text != "") {
+                            kısayolOluştur($"{textBox2.Text}", textBox1.Text, textBox3.Text);
+                        }
+                        else {
+                            MessageBox.Show("Hata: Dosya Adı Belirtmediniz.", "HATA!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else {
+                        MessageBox.Show("Hata: İkon Dosyası Bulunamadı.", "HATA!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else {
+                MessageBox.Show("Hata: Dosya Bulunamadı.", "HATA!!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK) textBox3.Text = openFileDialog2.FileName;
+        }
+    }
+}
